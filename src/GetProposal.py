db9409dde5308b007daf2062561f921bdb27ddd8
Gareth Murphy <garethcmurphy@gmail.com>
2019-06-14 13:54:08 +0200

    add read config

---
+++ src/GetProposal.py
@@ -0,0 +1,6 @@
+class GetProposal:
+    def __init__(self):
+        pass
+
+    def get(self):
+        pass
---
+++ src/SyncData.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import paramiko
+import os
+import pwd
+import json
+
+class SyncData:
+    def __init__(self):
+        self.fullname=pwd.getpwuid(os.getuid())[4]
+        self.username=self.fullname.replace(" ", ".").lower()
+        self.get_config()
+
+    def get_config(self):
+        with open('config.json') as json_file:  
+            data = json.load(json_file)
+            self.hostname=data['hostname']
+
+    def put(self):
+        hostname = self.hostname
+        print(hostname)
+        username = self.username
+        port = 22
+
+        mypath='/data/kafka-to-nexus/nicos000187.hdf'
+        mypath=os.getcwd()+'/x.txt'
+        print(mypath)
+        remotepath='/users/detector/experiments/V20/DEFAULT'
+        remotepath='/users/'+username+'/x.txt'
+        print(remotepath)
+
+        from os.path import expanduser
+        home = expanduser("~")
+        t = paramiko.Transport((hostname, port))
+        keyname=home+"/.ssh/id_ed25519"
+        print(username)
+        print(keyname)
+        ed25519_key = paramiko.Ed25519Key.from_private_key_file(keyname)
+        t.connect(username=username,  pkey=ed25519_key)
+        sftp = paramiko.SFTPClient.from_transport(t)
+        sftp.put(mypath, remotepath)
+
+
+if __name__ == "__main__":
+    sync = SyncData()
+    sync.put()
