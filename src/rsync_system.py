318529431b68732d8b301f06177ccbb059f60ff5
Gareth Murphy <garethcmurphy@gmail.com>
2019-06-21 04:58:55 +0200

    add docstrings

--- src/rsync_system.py
+++ src/rsync_system.py
@@ -11,11 +11,13 @@ class RsyncSystem(AbstractSync):
     file_array = []
 
     def fetch_files(self):
+        """fetch files"""
         file_mgr = GetFiles()
         file_mgr.set_base('./demo')
         self.file_array = file_mgr.get()
 
     def put(self):
+        """put files via system call to rsync"""
         print(self.input)
         local = " --include=".join(self.file_array)
         remote_dir = "login:v20"
@@ -23,11 +25,11 @@ class RsyncSystem(AbstractSync):
                         local, "--exclude='*'", remote_dir])
 
     def main(self):
+        """main program"""
         self.fetch_files()
         self.put()
 
 
 if __name__ == "__main__":
-    pass
-    rsync = RsyncSystem()
-    rsync.main()
+    R = RsyncSystem()
+    R.main()
