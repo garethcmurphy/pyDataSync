cbf21b34b8065129fb92497aa07647aaed43763d
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-29 09:44:14 +0200

    add docstring

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
+"""configure tests"""
 import sys
 sys.path.append("./src")
 
 
 def pytest_runtest_setup(item):
+    """unneeded test function"""
     # called for running each test in 'a' directory
     print("setting up", item)
